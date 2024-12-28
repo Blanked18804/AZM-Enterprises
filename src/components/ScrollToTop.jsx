923ed6026236c3c8ee690fb1cdd08035eda50368
Blanked18804 <zohaibmushtaq18821@gmail.com>
2024-12-28 15:31:03 +0500

    adjusted scroll to top button

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -37,7 +37,7 @@ function ScrollToTop() {
     }
 
   return (
-    <Link to='/' onClick={(event) => handleScorll(event)} className={`fixed bottom-16 right-8 rotate-180 p-2 bg-white border-2 border-[#000A2D] rounded-full cursor-pointer max-md:bottom-8 max-md:right-4 max-md:p-1 ${isHidden ? 'hidden' : ''}`}>
+    <Link to='/' onClick={(event) => handleScorll(event)} className={`fixed bottom-16 right-8 rotate-180 p-2 bg-white border-2 border-[#000A2D] rounded-full cursor-pointer max-md:p-1 ${isHidden ? 'hidden' : ''}`}>
         <img className='size-6 max-md:size-4' src={arrow} alt="to top" />
     </Link>
   )
