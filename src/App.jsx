e4a0fd0d64b6a75044cf6c571f30aee76367d5c6
Blanked18804 <zohaibmushtaq18821@gmail.com>
2025-02-13 22:59:39 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Product from './pages/Product'
+
+function usePageTracking() {
+  const location = useLocation() // Get current route
+
+  useEffect(() => {
+    if (window.gtag) { // Ensure gtag.js is loaded
+      window.gtag('config', 'G-F25BNS123Z', { // Send page view event
+        page_path: location.pathname,
+      })
+    }
+  }, [location]) // Run effect when location changes
+}
 
 function App() {
 
